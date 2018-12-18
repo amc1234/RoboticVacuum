6a5bdde624a6a6be77e53c04cb3457a971c54066
Susan <s529chen@edu.uwaterloo.com>
2018-12-18 10:04:41 -0500

    toggle

--- RoboticVacuum/app/src/main/java/roboticvacuum/roboticvacuum/MainActivity.java
+++ RoboticVacuum/app/src/main/java/roboticvacuum/roboticvacuum/MainActivity.java
@@ -2,17 +2,16 @@ package roboticvacuum.roboticvacuum;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
-import android.widget.ImageView;
 import android.widget.RelativeLayout;
-import android.widget.TextView;
+import android.widget.Switch;
 
 public class MainActivity extends Activity {
     RelativeLayout layout_joystick1, layout_joystick2;
-    ImageView image_joystick, image_border;
-    TextView textView1, textView2, textView3, textView4, textView5;
+    Switch toggle_vacuum;
 
     JoyStickClass js1, js2;
 
@@ -20,14 +19,9 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView1 = (TextView)findViewById(R.id.textView1);
-        textView2 = (TextView)findViewById(R.id.textView2);
-        textView3 = (TextView)findViewById(R.id.textView3);
-        textView4 = (TextView)findViewById(R.id.textView4);
-        textView5 = (TextView)findViewById(R.id.textView5);
-
-        layout_joystick1 = (RelativeLayout)findViewById(R.id.layout_joystick1);
-        layout_joystick2 = (RelativeLayout)findViewById(R.id.layout_joystick2);
+        layout_joystick1 = (RelativeLayout) findViewById(R.id.layout_joystick1);
+        layout_joystick2 = (RelativeLayout) findViewById(R.id.layout_joystick2);
+        toggle_vacuum = (Switch) findViewById(R.id.toggle_vacuum);
 
         js1 = new JoyStickClass(getApplicationContext(), layout_joystick1, R.drawable.image_button);
         js1.setStickSize(75, 75);
@@ -45,24 +39,28 @@ public class MainActivity extends Activity {
         js2.setOffset(35);
         js2.setMinimumDistance(50);
 
+        setupListener();
+    }
+
+    void setupListener() {
+        toggle_vacuum.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                if(toggle_vacuum.isChecked()) {
+                    // TODO: turn on vacuum
+                }
+                else {
+                    // TODO: turn off vacuum
+                }
+            }
+        });
+
         layout_joystick1.setOnTouchListener(new OnTouchListener() {
             public boolean onTouch(View arg0, MotionEvent arg1) {
                 js1.drawStick(arg1);
-                if(arg1.getAction() == MotionEvent.ACTION_DOWN || arg1.getAction() == MotionEvent.ACTION_MOVE) {
-
-                    int direction = js1.get8Direction();
-                    if(direction == JoyStickClass.STICK_UP) {
-                    } else if(direction == JoyStickClass.STICK_UPRIGHT) {
-                    } else if(direction == JoyStickClass.STICK_RIGHT) {
-                    } else if(direction == JoyStickClass.STICK_DOWNRIGHT) {
-                    } else if(direction == JoyStickClass.STICK_DOWN) {
-                    } else if(direction == JoyStickClass.STICK_DOWNLEFT) {
-                    } else if(direction == JoyStickClass.STICK_LEFT) {
-                    } else if(direction == JoyStickClass.STICK_UPLEFT) {
-                    } else if(direction == JoyStickClass.STICK_NONE) {
-                    }
-                } else if(arg1.getAction() == MotionEvent.ACTION_UP) {
-                }
+                changeSpeed(getSpeed(js1.getY()), getSpeed(js2.getY()));
                 return true;
             }
         });
@@ -70,23 +68,18 @@ public class MainActivity extends Activity {
         layout_joystick2.setOnTouchListener(new OnTouchListener() {
             public boolean onTouch(View arg0, MotionEvent arg1) {
                 js2.drawStick(arg1);
-                if(arg1.getAction() == MotionEvent.ACTION_DOWN || arg1.getAction() == MotionEvent.ACTION_MOVE) {
-                    int direction = js2.get8Direction();
-                    if(direction == JoyStickClass.STICK_UP) {
-                    } else if(direction == JoyStickClass.STICK_UPRIGHT) {
-                    } else if(direction == JoyStickClass.STICK_RIGHT) {
-                    } else if(direction == JoyStickClass.STICK_DOWNRIGHT) {
-                    } else if(direction == JoyStickClass.STICK_DOWN) {
-                    } else if(direction == JoyStickClass.STICK_DOWNLEFT) {
-                    } else if(direction == JoyStickClass.STICK_LEFT) {
-                    } else if(direction == JoyStickClass.STICK_UPLEFT) {
-                    } else if(direction == JoyStickClass.STICK_NONE) {
-                    }
-                } else if(arg1.getAction() == MotionEvent.ACTION_UP) {
-
-                }
+                changeSpeed(getSpeed(js1.getY()), getSpeed(js2.getY()));
                 return true;
             }
         });
+    }
+
+    float getSpeed(float jsMag) {
+        return jsMag;
+    }
+
+    void changeSpeed(float v1, float v2) {
+//        Log.d("f", "changeSpeed: " + String.valueOf(v1) + String.valueOf(v2));
+        // TODO: change speed
     }
 }
